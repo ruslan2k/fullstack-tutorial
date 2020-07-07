94b38b1a451dea4b6da4e9aec6aa3265f52bab95
Ruslan K <free.changer@gmail.com>
2020-07-07 21:51:25 +0200

    Added login mutation

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -2,6 +2,10 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const { createStore } = require('./utils');
 const resolvers = require('./resolvers');
+const isEmail = require('isemail');
+require('dotenv').config()
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 4000;
 
 const LaunchAPI = require('./datasources/launch');
 const UserAPI = require('./datasources/user');
@@ -9,14 +13,28 @@ const UserAPI = require('./datasources/user');
 const store = createStore();
 
 const server = new ApolloServer({
+  context: async ({ req }) => {
+    // simple auth check on every request
+    const auth = req.headers && req.headers.authorization || '';
+    const email = Buffer.from(auth, 'base64').toString('ascii');
+    if (!isEmail.validate(email)) return { user: null };
+    // find a user by their email
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = users && users[0] || null;
+    return { user: { ...user.dataValues } };
+  },
   typeDefs,
   resolvers,
+  engine: {
+    reportSchema: true,
+    debugPrintReports: true,
+  },
   dataSources: () => ({
     launchAPI: new LaunchAPI(),
     userAPI: new UserAPI({ store })
   })
 });
 
-server.listen().then(({ url }) => {
+server.listen(PORT, HOST).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
